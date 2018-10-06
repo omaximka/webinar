f27c2bd7664790453f88e924f4e9c4204cf72ea8
MIV <front.igor.m@gmail.com>
2018-10-06 12:40:23 +0500

    email send add

--- brokerige/callback-footer.php
+++ brokerige/callback-footer.php
@@ -1,14 +1,17 @@
 <?php
 
-    date_default_timezone_set('Asia/Yekaterinburg');
+    /*date_default_timezone_set('Asia/Yekaterinburg');*/
+    date_default_timezone_set('Europe/Moscow');
 
     $getForm = getParam('data-form');
+    $phone = 'not_phone';
 
     switch ($getForm) {
         case 'footer-form':
             $name = getParam('f-name');
             $phone = getParam('f-phone');
             $email = getParam('f-email');
+            $event_form = 'Форма регистрации внизу сайта';
             if (isset($name) && empty($name)) $errors['f-name'] = 'NOT_NAME';
             if (isset ($email) && empty($email)) $errors['f-email'] = 'NOT_EMAIL';
             if (isset ($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) $errors['f-email'] = 'NOT_EMAIL_VALIDATE';
@@ -16,6 +19,7 @@
         case 'content-form':
             $name = getParam('c-name');
             $email = getParam('c-email');
+            $event_form = 'Форма "Задать свой вопрос в режиме онлайн"';
             if (isset($name) && empty($name)) $errors['c-name'] = 'NOT_NAME';
             if (isset ($email) && empty($email)) $errors['c-email'] = 'Не указан адрес электронной почты';
             if (isset ($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) $errors['c-email'] = 'NOT_EMAIL_VALIDATE';
@@ -23,6 +27,7 @@
         case 'pop-form':
             $name = getParam('p-name');
             $email = getParam('p-email');
+            $event_form = 'Поп-ап форма в начале сайта';
             if (isset($name) && empty($name)) $errors['p-name'] = 'NOT_NAME';
             if (isset ($email) && empty($email)) $errors['p-email'] = 'NOT_EMAIL';
             if (isset ($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) $errors['p-email'] = 'NOT_EMAIL_VALIDATE';
@@ -34,17 +39,26 @@
         exit();
     }
 
-    /*
+    $body = date('d.m.Y')." | ".date('H:i')."<br><br>";
 
-        amoCRM
+    $body .= "На сайте ".$_SERVER['SERVER_NAME']." была заполнена форма регистрации."."<br><br>";
 
-        iSPRING
+    $body .= "Событие формы: ".$event_form."<br><br>";
 
-        Send Pulse и отправить письмо юзеру с его доступами для iSPRING $a = mail($email_address, $subject, $body, $header);
+    $body .= "ФИО: ".$name."<br><br>";
 
-    */
+    $body .= "Телефон: ".$phone."<br><br>";
 
-    $a = true;
+    $body .= "Email: ".$email."<br><br>";
+
+    $header = "Content-type: text/html; charset=\"utf-8\"\r\n";
+    $header .= "From: automail@rbs-24.ru\r\n";
+
+    $email_address = "sale@rbs-24.ru";
+
+    $subject = $_SERVER['SERVER_NAME'] . " - Регистрация пользователя";
+
+    $a = mail($email_address, $subject, $body, $header);
 
 echo json_encode(array('res' => $a ? 'ok' : 'errors', array('')));
 
